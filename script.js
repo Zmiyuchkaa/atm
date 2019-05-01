e8cb11fc5506ce7bcccf830132b705e5d768c4d3
Elena Rolskaya <e.rolskaya@kv.beets>
2019-05-01 21:09:41 +0300

    finished for today

--- script.js
+++ script.js
@@ -11,8 +11,13 @@
 - ругается на обращение к балансу в функции withdraw
 
 - нет проверки условия та ли карта в банкомате банка
-*/
 
+- не работает метод смены пароля - не распознает this.Card.pincode
+
+- Client методы: showTotalBalance() - подсчитывает баланс со всех карт и выводит в консоль.
+
+- все операции с балансом - сплошной баг
+*/
 
 // конструктор банка
 let Bank = function(name, accounts) {
@@ -44,9 +49,14 @@ let Card = function(account, balance, pincode, owner, Bank) {
     "bank": Bank
   }
 
+  obj.transferMoney = function(card, amount) {
+    this.Card.balance = this.Card.balance - amount;
+    card.balance = card.balance + amount; 
+    return this;
+  }
+
   return obj;
 }
-
 
 // конструктор банкомата
 let CashMachine = function(Bank, balance) {
@@ -56,22 +66,33 @@ let CashMachine = function(Bank, balance) {
   }
 
   obj.withdraw = function(Card, sum) {
+
     this.balance = this.balance - sum;
     if (this.balance < sum) {
       console.log("Недостаточно средств на счету")
     } return this;
-    // else if (this.CashMachine.Bank !== this.CashMachine.Bank) {
+    // else (this.CashMachine.Bank !== this.CashMachine.Bank) {
     //   console.log("Эта карта не может быть принята")
     // }
     
   }
     obj.add = function(Card, sum) {
+
       this.balance = this.balance + sum;
       if (sum >  this.balance) {
         console.log("Недостаточно средств в банкомате")
       } return this;
       
     } 
+
+    obj.changePIN = function(Card, oldPIN, newPIN) {
+
+      if (oldPIN == this.Card.pincode) {
+        this.Card.pincode = newPIN;
+        return this;
+      }
+      else (alert("You entered a wrong PIN"))
+    }
 
     return obj;
 }
@@ -86,59 +107,16 @@ let Client = function(name, cards=undefined) {
   return obj
 }
 
-// экземпляры банка
+
 let PrivatBank = Bank("PrivatBank", ["USD account", "UAH account", "deposit account", "credit card"]);
 let OTPBank = Bank("OTP Bank", ["USD account", "UAH account", "deposit account"]);
 
-
-
-
-/*
-- Банкомат методы:
-
-
-
-
-
-changePIN(card, old PIN, new PIN) - смена пинкода карты. Получает аргументом объект карты, пинкод от карты и 
-новый пинкод. Если пинкод карты не совпадает с тем, что был передан в метод, то вывести сообщение
-*/
-
-
-
-// экземпляры банкомата
 let CashMachine1 = CashMachine(PrivatBank, 20000);
 let CashMachine2 = CashMachine(OTPBank, 12764);
 
-
-
-/*
-- Банковская карточка (счет: integer, баланс: float, пинкод: string, владелец: класс Client, банк: класс Bank)
-методы:
-
-transferMoney(card, amount) - переводит с одной карты на другую. ВАЖНО! - при переводе деньги с объекта снимаются, 
-а в другой объект добавляются.
-*/
-
-
-
-
 let Client1 = Client("Elena Rolskaya");
+let Client2 = Client("Nick Gushchin");
 
 let Card1 = Card(PrivatBank.accounts[1], 2683, "10156", Client1, PrivatBank);
-let Client2 = Client("Nick Gushchin");
-CashMachine1.withdraw(Card1, 200);
-console.log(Card1);
+let Card2 = Card(OTPBank.accounts[1], 1565, "10156", Client1, OTPBank);
 
-let Card3 = PrivatBank.openAccount(Client1);
-console.log(Client1);
-console.log(Card1);
-console.log(Card3);
-
-
-/*
-- Client (имя: строка, cards: массив открытых счетов)
-методы:
-
-showTotalBalance() - подсчитывает баланс со всех карт и выводит в консоль.
-*/
