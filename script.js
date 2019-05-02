f951836fda44aae7baf1ff3b608814f05104548e
Elena Rolskaya <e.rolskaya@kv.beets>
2019-05-02 16:16:44 +0300

    fixed some bugs

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 /*
 Баги:
-
-- созданные при помощи метода карты не добавляются в массив клиента
-
-- ВАЖНО! - в одном банке можно открыть только один счет. 
-Если клиент делает это повторно, то вывести сообщение
-
 - closeAccount(card) - удаляет объект карточки из памяти (и объект карты из массива cards у клиента)
-
-- ругается на обращение к балансу в функции withdraw
-
-- нет проверки условия та ли карта в банкомате банка
-
-- не работает метод смены пароля - не распознает this.Card.pincode
-
 - Client методы: showTotalBalance() - подсчитывает баланс со всех карт и выводит в консоль.
-
-- все операции с балансом - сплошной баг
 */
 
 // конструктор банка
@@ -28,13 +13,18 @@ let Bank = function(name, accounts) {
   } 
 
   obj.openAccount = function(Client) {
-    this.Card = Object.create(Card1);
-    this.Client = Client;
-    // this.Client.push(obj);
-    if (this.Card.Bank == 3) { //                        !!! random условие
-      alert(`Dear ${this.Client.name}, you \ncan't open a new card in the same Bank.Sorry.`)
+    if (Client.cards.length > 0) {
+      for (i = 0; i <= Client.cards.length; i++) {
+      if (Client.cards[i] == Bank.name) {
+        alert(`Dear ${Client.name}, you \ncan't open a new card in the same Bank.Sorry.`)
+      }
     }
-    return this;
+    }
+    else {
+      this.Card = Object.create(Card1);
+      Client.cards.push(obj);
+      return this;
+    }
   }
   return obj;
 }
@@ -61,47 +51,55 @@ let Card = function(account, balance, pincode, owner, Bank) {
 // конструктор банкомата
 let CashMachine = function(Bank, balance) {
   let obj = {
-    "bank": Bank,
-    "balance": balance
+      "bank": Bank,
+      "balance": balance
   }
-
   obj.withdraw = function(Card, sum) {
-
-    this.balance = this.balance - sum;
-    if (this.balance < sum) {
-      console.log("Недостаточно средств на счету")
-    } return this;
-    // else (this.CashMachine.Bank !== this.CashMachine.Bank) {
-    //   console.log("Эта карта не может быть принята")
-    // }
-    
-  }
-    obj.add = function(Card, sum) {
-
-      this.balance = this.balance + sum;
-      if (sum >  this.balance) {
-        console.log("Недостаточно средств в банкомате")
-      } return this;
-      
-    } 
-
-    obj.changePIN = function(Card, oldPIN, newPIN) {
-
-      if (oldPIN == this.Card.pincode) {
-        this.Card.pincode = newPIN;
-        return this;
+      if (this.bank !== Card.bank) {
+          console.log("Эта карта не может быть принята")
+      } else if (this.balance < sum) {
+          console.log("Недостаточно средств на счету")
+      } else {
+          this.balance = this.balance - sum;
+          return this;
       }
-      else (alert("You entered a wrong PIN"))
-    }
-
-    return obj;
+  }
+  obj.add = function(Card, sum) {
+      this.balance = this.balance + sum;
+      if (sum > this.balance) {
+          console.log("Недостаточно средств в банкомате")
+      }
+      return this;
+  }
+  obj.changePIN = function(Card, oldPIN, newPIN) {
+      if (oldPIN == Card.pincode) {
+          Card.pincode = newPIN;
+          return this;
+      } else(alert("You entered a wrong PIN"))
+  }
+  return obj;
 }
+
+// closeAccount(card) - удаляет объект карточки из памяти (и объект карты из массива cards у клиента)
 
 // конструктов клиентов
 let Client = function(name, cards=undefined) {
   let obj = {
     "name": name,
-    "cards": cards
+    "cards": []
+  }
+
+  // obj.closeAccount = function(card) {
+  //   for (i = 0; i < Client.cards.length; i++) {
+
+  // }
+  obj.showTotalBalance = function() {
+    let total = 0;
+    for (i = 0; i < Client.cards.balance.length; i++) {
+      console.log("I'm here")
+      total += Client.cards[i].balance.length;
+      console.log(total);
+    }
   }
 
   return obj
@@ -117,6 +115,13 @@ let CashMachine2 = CashMachine(OTPBank, 12764);
 let Client1 = Client("Elena Rolskaya");
 let Client2 = Client("Nick Gushchin");
 
-let Card1 = Card(PrivatBank.accounts[1], 2683, "10156", Client1, PrivatBank);
-let Card2 = Card(OTPBank.accounts[1], 1565, "10156", Client1, OTPBank);
+let Card1 = Card("UAH account", 1567, "10156", Client1, PrivatBank);
+console.log(Card1);
+
+
+PrivatBank.openAccount(Client1);
+OTPBank.openAccount(Client1);
+console.log(Client1.cards);
+
+
 
